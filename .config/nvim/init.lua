e129c9f8904f637a7b69928dce8684faf04be159
Phi Long Danh <longdp@gdvietnam.com>
2022-06-08 22:31:57 +0700

    v1.0

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,43 @@
-vim.cmd [[
-  syntax enable
-  colorscheme moonfly
-]]
+local set = vim.opt
 
--- Format on save
-vim.cmd [[
-  autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-]]
+set.mouse = 'a'
+set.ai = true
+set.ruler = true
+set.number = true
+set.hlsearch = true
+set.expandtab = true
+set.termguicolors = true
+set.tabstop = 2
+set.shiftwidth = 2
 
+-- mappings.
+-- see `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- better window navigation
+local keymap = vim.api.nvim_set_keymap
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+
+-- telescope keymap
+keymap("n", "ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
+keymap("n", "fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
+keymap("n", "fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
+keymap("n", "fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
+
+-- automatically install packer
 local fn = vim.fn
-
--- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system {
@@ -25,7 +52,13 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd [[packadd packer.nvim]]
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
+-- color schemes
+vim.cmd [[colorscheme moonfly]]
+
+-- format on save
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+
+-- autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd [[
   augroup packer_user_config
     autocmd!
@@ -33,60 +66,18 @@ vim.cmd [[
   augroup end
 ]]
 
-local set = vim.opt
-
-set.encoding = 'UTF-8'
-set.termguicolors = true
-set.tabstop = 2
-set.shiftwidth = 2
-set.expandtab = true
-set.ai = true
-set.number = true
-set.hlsearch = true
-set.ruler = true
-set.mouse = 'a'
-
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
-
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
-keymap("n", "<C-b>", ":NvimTreeToggle<cr>", opts)
-
--- Telescope keymap
-keymap("n", "ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
-keymap("n", "fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
-keymap("n", "fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
-keymap("n", "fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
-
-
--- Use an on_attach function to only map the following keys
+-- use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
+  if client.name == 'tsserver' then
     client.resolved_capabilities.document_formatting = false
   end
-  
-  -- Enable completion triggered by <c-x><c-o>
+
+  -- enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  -- mappings.
+  -- see `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
@@ -102,55 +93,60 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
 end
 
-
+-- INSTALL PLUGINS WITH PACKER
 local use = require('packer').use
 require('packer').startup(function()
-  use "williamboman/nvim-lsp-installer" -- Lsp Installer
-  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
-  use "kyazdani42/nvim-web-devicons"
-  use 'bluz71/vim-moonfly-colors' -- Colors scheme
-  use 'windwp/nvim-autopairs' -- Autopairs
-  use 'windwp/nvim-ts-autotag' -- Autotag
-  use "terrortylor/nvim-comment" -- Comment
-  use 'jose-elias-alvarez/null-ls.nvim' -- Formatting
-  use {
-    "nvim-treesitter/nvim-treesitter", -- Treesitter
-    run = ":TSUpdate",
-  }
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup()
-    end
-  }
+  use('wbthomason/packer.nvim')
+  use('neovim/nvim-lspconfig')
+  use('williamboman/nvim-lsp-installer')
+  use('hrsh7th/nvim-cmp')
+  use('hrsh7th/cmp-nvim-lsp')
+  use('saadparwaiz1/cmp_luasnip')
+  use('L3MON4D3/LuaSnip')
+  use('kyazdani42/nvim-web-devicons')
+  use('bluz71/vim-moonfly-colors')
+  use('windwp/nvim-autopairs')
+  use('windwp/nvim-ts-autotag')
+  use('terrortylor/nvim-comment')
+  use('jose-elias-alvarez/null-ls.nvim')
+  use('nvim-treesitter/nvim-treesitter')
+  use('lewis6991/gitsigns.nvim')
   use {
     'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    },
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
   use {
-    'akinsho/bufferline.nvim', 
-    tag = "v2.*", 
-    requires = 'kyazdani42/nvim-web-devicons'
+    'akinsho/bufferline.nvim',
+    tag = 'v2.*',
   }
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = 'nvim-lua/plenary.nvim'
   }
 end)
 
-require("nvim-lsp-installer").setup({
-  -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-  automatic_installation = true, 
+-- add additional capabilities supported by nvim-cmp
+local make_client_capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require('cmp_nvim_lsp')
+    .update_capabilities(make_client_capabilities)
+
+-- use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'tsserver' }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+require('nvim-lsp-installer').setup {
+  -- automatically detect which servers to install
+  -- (based on which servers are set up via lspconfig)
+  automatic_installation = true,
   ui = {
     icons = {
       server_installed = "✓",
@@ -158,31 +154,49 @@ require("nvim-lsp-installer").setup({
       server_uninstalled = "✗"
     }
   }
+}
+
+require('lspconfig')['sumneko_lua'].setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+    },
+  },
+}
+
+require('gitsigns').setup {}
+require('nvim-tree').setup {}
+require('bufferline').setup {}
+require('nvim_comment').setup {}
+require('nvim-autopairs').setup {}
+require('nvim-web-devicons').setup {}
+
+require('null-ls').setup({
+  sources = {
+    require('null-ls').builtins.completion.spell,
+    require('null-ls').builtins.formatting.stylua,
+    require('null-ls').builtins.diagnostics.eslint,
+    require('null-ls').builtins.formatting.prettier,
+  },
 })
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver' }
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
-end
+require('nvim-treesitter.configs').setup {
+  autotag = {
+    enable = true,
+  },
+  highlight = {
+    enable = true,
+  },
+}
 
 -- luasnip setup
-local luasnip = require 'luasnip'
+local luasnip = require('luasnip')
 
 -- nvim-cmp setup
-local cmp = require 'cmp'
+local cmp = require('cmp')
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -222,57 +236,9 @@ cmp.setup {
   },
 }
 
-require'nvim-tree'.setup {}
-require("bufferline").setup{}
-require('nvim_comment').setup()
-require('nvim-autopairs').setup{}
-require 'nvim-web-devicons'.setup {}
-
-require("null-ls").setup({
-  sources = {
-    require("null-ls").builtins.formatting.stylua,
-    require("null-ls").builtins.formatting.prettier,
-    require("null-ls").builtins.diagnostics.eslint,
-    require("null-ls").builtins.completion.spell,
-  },
-})
-
-require'nvim-treesitter.configs'.setup {
-  autotag = {
-    enable = true,
-  },
-  -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "c", "rust" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
-
--- If you want insert `(` after select function or method item
+-- if you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
-
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
 
 -- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
-cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
+cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = 'racket'
