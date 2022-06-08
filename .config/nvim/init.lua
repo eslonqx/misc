c9496001bee56fac906464266663b8f2a7393c2e
Phi Long Danh <longdp@gdvietnam.com>
2022-06-08 11:16:04 +0700

    Update autopairs and autotag

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -110,6 +110,8 @@ require('packer').startup(function()
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
   use "kyazdani42/nvim-web-devicons"
   use 'bluz71/vim-moonfly-colors' -- Colors scheme
+  use 'windwp/nvim-autopairs' -- Autopairs
+  use 'windwp/nvim-ts-autotag' -- Autotag
   use {
     "nvim-treesitter/nvim-treesitter", -- Treesitter
     run = ":TSUpdate",
@@ -208,5 +210,45 @@ cmp.setup {
 
 require'nvim-tree'.setup {}
 require("bufferline").setup{}
+require('nvim-autopairs').setup{}
 require 'nvim-web-devicons'.setup {}
-require 'nvim-treesitter.configs'.setup {}
+
+require'nvim-treesitter.configs'.setup {
+  autotag = {
+    enable = true,
+  },
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "rust" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+
+
+-- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
+cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
