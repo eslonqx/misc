e8498101e4dbd6a8383853730e7b0c3f12968a2e
Phi Long Danh <longdp@gdvietnam.com>
2022-06-08 13:41:25 +0700

    Add git

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -117,6 +117,12 @@ require('packer').startup(function()
     run = ":TSUpdate",
   }
   use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+  use {
     'kyazdani42/nvim-tree.lua',
     requires = {
       'kyazdani42/nvim-web-devicons', -- optional, for file icon
